2ed194d3fc706baa4f078ada1819fd889296b38b
Sebastian <jsninoc94@gmail.com>
2018-09-02 13:43:46 -0500

    MP-04 #5 finished the CRUD of Knowledge

--- src/main/java/co/edu/unal/vsacode/moodleplz/controllers/KnowledgeController.java
+++ src/main/java/co/edu/unal/vsacode/moodleplz/controllers/KnowledgeController.java
@@ -19,9 +19,20 @@ public class KnowledgeController {
         return knowledgeService.getKnowledge();
     }
 
+    @PutMapping("/{id}")
+    public Knowledge updateCustomer(@PathVariable (name="id") String id, @RequestBody Knowledge newKnowledge){
+        newKnowledge.setId(id);
+        return knowledgeService.updateKnowledge(newKnowledge);
+    }
+
     @PostMapping
-    public Knowledge saveKwnoledge(@RequestBody Knowledge newKnowledge){
+    public Knowledge saveKnowledge(@RequestBody Knowledge newKnowledge){
         return knowledgeService.saveKnowledge(newKnowledge);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteKnowledge(@PathVariable (name = "id") String id){
+        knowledgeService.deleteKnowledge(id);
     }
 
 }
--- src/main/java/co/edu/unal/vsacode/moodleplz/services/KnowledgeService.java
+++ src/main/java/co/edu/unal/vsacode/moodleplz/services/KnowledgeService.java
@@ -2,9 +2,11 @@ package co.edu.unal.vsacode.moodleplz.services;
 
 import co.edu.unal.vsacode.moodleplz.models.Knowledge;
 import co.edu.unal.vsacode.moodleplz.repositories.KnowledgeRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -13,11 +15,31 @@ public class KnowledgeService {
     @Autowired
     private KnowledgeRepository knowledgeRepository;
 
+    public List<Knowledge> knowledges = new ArrayList<>();
+
     public List<Knowledge> getKnowledge() {
         return knowledgeRepository.findAll();
     }
 
+    public Knowledge getKnowledgeById(String id){
+        return knowledgeRepository.findById(id).orElse(null);
+    }
+
     public Knowledge saveKnowledge (Knowledge newKnowledge){
         return knowledgeRepository.save(newKnowledge);
+    }
+
+    public Knowledge updateKnowledge(Knowledge newKnowledge){
+        Knowledge knowledge = this.getKnowledgeById(newKnowledge.getId());
+
+        if (knowledge != null){
+            BeanUtils.copyProperties(newKnowledge, knowledge);
+        }
+
+        return knowledgeRepository.save(newKnowledge);
+    }
+
+    public void deleteKnowledge(String id){
+        knowledgeRepository.delete(this.getKnowledgeById(id));
     }
 }
