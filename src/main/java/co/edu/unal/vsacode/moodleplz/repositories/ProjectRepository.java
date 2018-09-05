3aec624a7fcf8b978f407399b57ed04dc05a0f42
HP-ADRO\ANGEL RIOS <adrioso@unal.edu.co>
2018-09-04 21:57:16 -0500

    MP-00 Added @Repository annotations to all repositories

--- src/main/java/co/edu/unal/vsacode/moodleplz/repositories/ProjectRepository.java
+++ src/main/java/co/edu/unal/vsacode/moodleplz/repositories/ProjectRepository.java
@@ -3,9 +3,10 @@ package co.edu.unal.vsacode.moodleplz.repositories;
 
 import co.edu.unal.vsacode.moodleplz.models.Project;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface ProjectRepository extends MongoRepository<Project, String> {
     Optional<Project> findByName(String name);
 }
--- src/main/java/co/edu/unal/vsacode/moodleplz/repositories/StaffMemberRepository.java
+++ src/main/java/co/edu/unal/vsacode/moodleplz/repositories/StaffMemberRepository.java
@@ -2,7 +2,9 @@ package co.edu.unal.vsacode.moodleplz.repositories;
 
 import co.edu.unal.vsacode.moodleplz.models.StaffMember;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StaffMemberRepository extends MongoRepository<StaffMember, String> {
 
 }
