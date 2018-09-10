8635a591ac49c6edbef528ff08a6119a476a0baf
HP-ADRO\ANGEL RIOS <adrioso@unal.edu.co>
2018-09-10 18:55:59 -0500

    MP-12 Login and user management added constructor to the models

--- src/main/java/co/edu/unal/vsacode/moodleplz/controllers/GroupController.java
+++ src/main/java/co/edu/unal/vsacode/moodleplz/controllers/GroupController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/groups")
+@RequestMapping("/group")
 public class GroupController {
 
     @Autowired
