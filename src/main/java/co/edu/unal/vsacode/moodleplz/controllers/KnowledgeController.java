bb7f94890fc040473a8f7c991c9e42f26c2ac866
Sebastian <jsninoc94@gmail.com>
2018-09-16 20:34:28 -0500

    MP-00 little changes in skill and knowledge

--- src/main/java/co/edu/unal/vsacode/moodleplz/controllers/KnowledgeController.java
+++ src/main/java/co/edu/unal/vsacode/moodleplz/controllers/KnowledgeController.java
@@ -19,6 +19,9 @@ public class KnowledgeController {
         return knowledgeService.getKnowledge();
     }
 
+    @GetMapping("/{id}")
+    Knowledge getKnowledge(@PathVariable String id){return knowledgeService.getKnowledgeById(id);}
+
     @PutMapping("/{id}")
     public Knowledge updateCustomer(@PathVariable (name="id") String id, @RequestBody Knowledge newKnowledge){
         newKnowledge.setId(id);
