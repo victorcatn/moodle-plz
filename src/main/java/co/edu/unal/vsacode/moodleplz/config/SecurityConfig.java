e04f276b2ea04b00cf75e2fbe7e4c50c25b43258
Pablo Muñoz <pmunoz@cidenet.com.co>
2018-10-02 17:30:21 -0500

    MP-00 User password work around [no Bycrpt]

--- src/main/java/co/edu/unal/vsacode/moodleplz/config/SecurityConfig.java
+++ src/main/java/co/edu/unal/vsacode/moodleplz/config/SecurityConfig.java
@@ -19,6 +19,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception{
         http
+            .cors().and()
             .csrf().disable()
             .authorizeRequests().anyRequest().authenticated()
             .and().httpBasic()
