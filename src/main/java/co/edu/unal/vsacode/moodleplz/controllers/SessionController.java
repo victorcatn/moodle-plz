e95534dda87d73ee310f2fc964bc8ab6e1b6eaf4
Sebastian <jsninoc94@gmail.com>
2018-10-05 15:26:18 -0500

    MP-00 integration with security of front end

--- src/main/java/co/edu/unal/vsacode/moodleplz/controllers/SessionController.java
+++ src/main/java/co/edu/unal/vsacode/moodleplz/controllers/SessionController.java
@@ -1,22 +1,37 @@
 package co.edu.unal.vsacode.moodleplz.controllers;
 
+import org.springframework.security.core.userdetails.User;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.security.Principal;
+import java.util.Base64;
 import java.util.Collections;
 import java.util.Map;
 
 @RestController
 public class SessionController {
-    @RequestMapping("/user")
+    /*@RequestMapping("/user")
     public Principal user(Principal user){
         return user;
+    }*/
+
+    @RequestMapping("/user")
+    public Principal user(HttpServletRequest request){
+        String authToken = request.getHeader("Authorization").substring("Basic".length()).trim();
+        return () -> new String(Base64.getDecoder().decode(authToken)).split(":")[0];
     }
 
     @RequestMapping("/token")
     public Map<String,String> token(HttpSession session) {
         return Collections.singletonMap("token", session.getId());
+    }
+
+    @RequestMapping("/login")
+    public boolean login(@RequestBody User user){
+        return user.getUsername().equals("document") && user.getPassword().equals("password");
     }
 }
