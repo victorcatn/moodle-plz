09a8e11030643731f01a080913be11a9d875fe28
HP-ADRO\ANGEL RIOS <adrioso@unal.edu.co>
2018-10-08 20:04:58 -0500

    MP-00 Unit test Algorithm

--- src/test/java/co/edu/unal/vsacode/moodleplz/services/AlgorithmServiceTest.java
+++ src/test/java/co/edu/unal/vsacode/moodleplz/services/AlgorithmServiceTest.java
@@ -1,17 +1,21 @@
 package co.edu.unal.vsacode.moodleplz.services;
 
 import co.edu.unal.vsacode.moodleplz.models.*;
+import org.junit.Assert;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
+@SpringBootTest
 class AlgorithmServiceTest {
 
-    AlgorithmService algorithmService = new AlgorithmService();
+    private AlgorithmService algorithmService = new AlgorithmService();
 
     @BeforeEach
     void setUp() {
@@ -25,19 +29,19 @@ class AlgorithmServiceTest {
     void generateAlgorithm() {
         List<StaffMember> staffMembers = new ArrayList<>();
 
-        /**--------------------------------------------------Test 1---------------------------------------------------*/
+        /*--------------------------------------------------Test 1---------------------------------------------------*/
 
         Project project1 = new Project(
                 "P00000000000000000000001",
                 "test",
                 null,
                 null,
-                new ArrayList<SkillScore>(Arrays.asList(
+                new ArrayList<>(Arrays.asList(
                         new SkillScore("5ba07565291ec12a00df060d", 10),
                         new SkillScore("5b96f1b0291ec123846beef4", 10),
                         new SkillScore("5ba07559291ec12a00df060c", 10)
                 )),
-                new ArrayList<KnowledgeScore>(Arrays.asList(
+                new ArrayList<>(Arrays.asList(
                         new KnowledgeScore("5b96e6b4291ec11abcebf6ec",10),
                         new KnowledgeScore("5b96e6cb291ec11abcebf6ed",10),
                         new KnowledgeScore("5b96e69f291ec11abcebf6eb", 10)
@@ -54,10 +58,10 @@ class AlgorithmServiceTest {
                 "Castañeda Osorno",
                 false,
                 true,
-                new ArrayList<SkillScore>(Arrays.asList(
+                new ArrayList<>(Collections.singletonList(
                         new SkillScore("5ba07565291ec12a00df060d", 10)
                 )),
-                new ArrayList<KnowledgeScore>(Arrays.asList(
+                new ArrayList<>(Arrays.asList(
                         new KnowledgeScore("5b96e6b4291ec11abcebf6ec",10),
                         new KnowledgeScore("5b96e6cb291ec11abcebf6ed",10)
                 ))
@@ -76,14 +80,14 @@ class AlgorithmServiceTest {
                 "Castañeda Osorno",
                 false,
                 true,
-                new ArrayList<SkillScore>(Arrays.asList(
+                new ArrayList<>(Arrays.asList(
                         new SkillScore("5ba07565291ec12a00df060d", 55),
                         new SkillScore("5b96f1b0291ec123846beef4", 55),
                         new SkillScore("5ba07559291ec12a00df060c", 55),
                         new SkillScore("5ba07559291ec12a00df06012", 100),
                         new SkillScore("5ba07559291ec12a00df06023", 55)
                 )),
-                new ArrayList<KnowledgeScore>(Arrays.asList(
+                new ArrayList<>(Arrays.asList(
                         new KnowledgeScore("5b96e6b4291ec11abcebf6ec",55),
                         new KnowledgeScore("5b96e6cb291ec11abcebf6ed",55),
                         new KnowledgeScore("5b96e69f291ec11abcebf6eb", 55)
@@ -93,16 +97,14 @@ class AlgorithmServiceTest {
 
 
         Algorithm algorithm = algorithmService.generateAlgorithm(staffMembers, project1);
-        //TODO assertEquals
 
-        System.out.println(algorithm.getSuggestedStaffMember() + " suggested");
-        System.out.println(algorithm.getOthers() + " others");
-        System.out.println(algorithm.getNonSuggestedStaffMember() + " non suggested");
-        System.out.println("");
+        Assert.assertTrue(algorithm.getSuggestedStaffMember().contains(staffMember2));
+        Assert.assertTrue(algorithm.getOthers().contains(staffMember1));
+        Assert.assertTrue(algorithm.getNonSuggestedStaffMember().isEmpty());
 
 
 
-        /**--------------------------------------------------Test 2---------------------------------------------------*/
+        /*--------------------------------------------------Test 2---------------------------------------------------*/
 
 
         /*StaffMember3 cumple la condicion del nodo 1 y del nodo 2 pero en muy poca medida
@@ -116,11 +118,11 @@ class AlgorithmServiceTest {
                 "Castañeda Osorno",
                 false,
                 true,
-                new ArrayList<SkillScore>(Arrays.asList(
+                new ArrayList<>(Collections.singletonList(
                         new SkillScore("5ba07565291ec12a00df060d", 50)
                 )),
-                new ArrayList<KnowledgeScore>(Arrays.asList(
-                        new KnowledgeScore("5b96e6b4291ec11abcebf6ec",50)
+                new ArrayList<>(Collections.singletonList(
+                        new KnowledgeScore("5b96e6b4291ec11abcebf6ec", 50)
                 ))
         );
         staffMembers.add(staffMember3);
@@ -136,19 +138,165 @@ class AlgorithmServiceTest {
                 "Castañeda Osorno",
                 false,
                 true,
-                new ArrayList<SkillScore>(Arrays.asList(
+                new ArrayList<>(Collections.singletonList(
                         new SkillScore("5ba07565291ec12a00df060a", 50)
                 )),
-                new ArrayList<KnowledgeScore>(Arrays.asList(
-                        new KnowledgeScore("5b96e6b4291ec11abcebf6ea",50)
+                new ArrayList<>(Collections.singletonList(
+                        new KnowledgeScore("5b96e6b4291ec11abcebf6ea", 50)
                 ))
         );
         staffMembers.add(staffMember4);
 
         algorithm = algorithmService.generateAlgorithm(staffMembers, project1);
-        //TODO assertEquals
-        System.out.println(algorithm.getSuggestedStaffMember() + " suggested 2");
-        System.out.println(algorithm.getOthers() + " others 2");
-        System.out.println(algorithm.getNonSuggestedStaffMember() + " non suggested 2");
+
+        Assert.assertTrue(algorithm.getSuggestedStaffMember().contains(staffMember2));
+        Assert.assertFalse(algorithm.getSuggestedStaffMember().contains(staffMember3)
+                || algorithm.getSuggestedStaffMember().contains(staffMember4)
+                || algorithm.getSuggestedStaffMember().contains(staffMember1));
+        Assert.assertTrue(algorithm.getOthers().contains(staffMember1)
+                && algorithm.getOthers().contains(staffMember3));
+        Assert.assertFalse(algorithm.getNonSuggestedStaffMember().isEmpty());
+        Assert.assertTrue(algorithm.getNonSuggestedStaffMember().contains(staffMember4));
+
+        /*--------------------------------------------------Test 3---------------------------------------------------*/
+
+        Project project2 = new Project();
+
+        algorithm = algorithmService.generateAlgorithm(staffMembers, project2);
+
+        Assert.assertTrue(algorithm.getSuggestedStaffMember().isEmpty());
+        Assert.assertTrue(algorithm.getOthers().isEmpty());
+        Assert.assertEquals(4, algorithm.getNonSuggestedStaffMember().size());
+
+        /*--------------------------------------------------Test 4---------------------------------------------------*/
+
+        Project project3 = new Project();
+        List<StaffMember> staffMembers2 = new ArrayList<>();
+
+        algorithm = algorithmService.generateAlgorithm(staffMembers2, project3);
+
+        Assert.assertTrue(algorithm.getSuggestedStaffMember().isEmpty());
+        Assert.assertTrue(algorithm.getNonSuggestedStaffMember().isEmpty());
+        Assert.assertTrue(algorithm.getOthers().isEmpty());
+
+        /*--------------------------------------------------Test 5---------------------------------------------------*/
+
+        Project project4 = new Project(
+                "P00000000000000000000004",
+                "test",
+                null,
+                null,
+                new ArrayList<>(Arrays.asList(
+                        new SkillScore("5ba07565291ec12a00df060d", 10),
+                        new SkillScore("5b96f1b0291ec123846beef4", 10),
+                        new SkillScore("5ba07559291ec12a00df060c", 10)
+                )),
+                new ArrayList<>(Arrays.asList(
+                        new KnowledgeScore("5b96e6b4291ec11abcebf6ec",10),
+                        new KnowledgeScore("5b96e6cb291ec11abcebf6ed",10),
+                        new KnowledgeScore("5b96e69f291ec11abcebf6eb", 10)
+                )),
+                null);
+
+        List<StaffMember> staffMembers3 = new ArrayList<>();
+
+        algorithm = algorithmService.generateAlgorithm(staffMembers3, project4);
+
+        Assert.assertTrue(algorithm.getSuggestedStaffMember().isEmpty());
+        Assert.assertTrue(algorithm.getNonSuggestedStaffMember().isEmpty());
+        Assert.assertTrue(algorithm.getOthers().isEmpty());
+
+        /*--------------------------------------------------Test 5---------------------------------------------------*/
+        Project project5 = new Project(
+                "P00000000000000000000004",
+                "test",
+                null,
+                null,
+                new ArrayList<>(Arrays.asList(
+                        new SkillScore("5ba07565291ec12a00df060d", 10),
+                        new SkillScore("5b96f1b0291ec123846beef4", 10),
+                        new SkillScore("5ba07559291ec12a00df060c", 10)
+                )),
+                new ArrayList<>(Arrays.asList(
+                        new KnowledgeScore("5b96e6b4291ec11abcebf6ec",10),
+                        new KnowledgeScore("5b96e6cb291ec11abcebf6ed",10),
+                        new KnowledgeScore("5b96e69f291ec11abcebf6eb", 10)
+                )),
+                null);
+
+        List<StaffMember> staffMembers4 = new ArrayList<>();
+
+        StaffMember staffMember5 = new StaffMember(
+                "000000000000000000000005",
+                "1152464229",
+                "riosangel8@gmail.com",
+                "5233801.",
+                "Paola Andrea",
+                "Castañeda Osorno",
+                false,
+                true,
+                new ArrayList<>(Collections.singletonList(
+                        new SkillScore("5ba07565291ec12a00df060a", 50)
+                )),
+                new ArrayList<>(Collections.singletonList(
+                        new KnowledgeScore("5b96e6b4291ec11abcebf6ea", 50)
+                ))
+        );
+        staffMembers4.add(staffMember5);
+
+        algorithm = algorithmService.generateAlgorithm(staffMembers4, project5);
+
+        Assert.assertTrue(algorithm.getSuggestedStaffMember().isEmpty());
+        Assert.assertTrue(algorithm.getNonSuggestedStaffMember().contains(staffMember5));
+        Assert.assertTrue(algorithm.getOthers().isEmpty());
+
+        /*--------------------------------------------------Test 6---------------------------------------------------*/
+        Project project6 = new Project(
+                "P00000000000000000000004",
+                "test",
+                null,
+                null,
+                new ArrayList<>(Arrays.asList(
+                        new SkillScore("5ba07565291ec12a00df060d", 10),
+                        new SkillScore("5b96f1b0291ec123846beef4", 10),
+                        new SkillScore("5ba07559291ec12a00df060c", 10)
+                )),
+                new ArrayList<>(Arrays.asList(
+                        new KnowledgeScore("5b96e6b4291ec11abcebf6ec",10),
+                        new KnowledgeScore("5b96e6cb291ec11abcebf6ed",10),
+                        new KnowledgeScore("5b96e69f291ec11abcebf6eb", 10)
+                )),
+                null);
+
+        List<StaffMember> staffMembers5 = new ArrayList<>();
+
+        StaffMember staffMember6 = new StaffMember(
+                "000000000000000000000005",
+                "1152464229",
+                "riosangel8@gmail.com",
+                "5233801.",
+                "Paola Andrea",
+                "Castañeda Osorno",
+                false,
+                true,
+                new ArrayList<>(Arrays.asList(
+                        new SkillScore("5ba07565291ec12a00df060d", 10),
+                        new SkillScore("5b96f1b0291ec123846beef4", 10),
+                        new SkillScore("5ba07559291ec12a00df060c", 10)
+                )),
+                new ArrayList<>(Arrays.asList(
+                        new KnowledgeScore("5b96e6b4291ec11abcebf6ec",10),
+                        new KnowledgeScore("5b96e6cb291ec11abcebf6ed",10),
+                        new KnowledgeScore("5b96e69f291ec11abcebf6eb", 10)
+                ))
+        );
+        staffMembers5.add(staffMember6);
+
+        algorithm = algorithmService.generateAlgorithm(staffMembers5, project6);
+
+        Assert.assertTrue(algorithm.getSuggestedStaffMember().contains(staffMember6));
+        Assert.assertTrue(algorithm.getNonSuggestedStaffMember().isEmpty());
+        Assert.assertTrue(algorithm.getOthers().isEmpty());
+
     }
 }
